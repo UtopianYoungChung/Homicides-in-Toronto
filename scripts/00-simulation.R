2848c4da9126fc5c75411ba7f0d6060c1e7f2b65
Joseph Chung <yj.chung@mail.utoronto.ca>
2023-02-04 12:33:18 -0500

    Update 00-simulation.R
    Debugging syntax: function 'here::here' was added from lines 72 to 85 where the working directory was showing at false

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -15,29 +15,20 @@ library(patchwork)
 library(janitor)
 library(reshape2)
 
+
 #### Clean the data and only take the information we wish to use ####
 homicides_cleaned <- read_csv(here::here("inputs/data/homicides_data_unclean.csv"))
 homicides_cleaned %>%  
   select(Occurrence_year, Homicide_Type,Neighbourhood)
 head(homicides_cleaned)
 
-
-#### Test the homicides_cleaned dataset ####
-homicides_cleaned$Occurrence_year |> min() == 2004
-homicides_cleaned$Occurrence_year |> max() == 2020
-homicides_cleaned$Occurrence_year |> class() == "numeric"
-homicides_cleaned$Occurrence_year |>
-  unique() |>
-  length() == 1166
-
-
 ### Group by Occurrence year and count the numbers ###
-homicides_count <- homicides_cleaned %>%
+homicides_count <- homicides_cleaned%>%
   group_by(Occurrence_year) %>%
   dplyr:: count(Homicide_Type, name = 'ID')
 
 ### transform long to wide format###
-homicides_each_year = dcast(homicides_count, Occurrence_year~Homicide_Type, value.var = "ID")
+homicides_each_year = dcast(homicides_count, Occurrence_year~Homicide_Type, value.var = 'ID')
 
 
 ### Group by Neighborhood and count the number of Homicide type to summarise the total ### 
@@ -65,29 +56,39 @@ Homicides_compare <- homicides_cleaned %>%
 Homicides_compare_filtered <- Homicides_compare%>%
   filter(ID >= 15)
 
+#### Test the homicides_cleaned dataset ####
+# If there is TRUE outputting, then test is assumed to be passed
+# All of the test are passing with return value of True
+homicides_cleaned$Occurrence_year |> min() == 2004
+homicides_cleaned$Occurrence_year |> max() == 2020
+homicides_cleaned$Occurrence_year |> class() == "numeric"
+homicides_cleaned$Occurrence_year |> length() == 1166
+homicides_cleaned$Division |> class() == "character"
+homicides_cleaned$Division |> length() == 1166
+
 
 #### Save the data in our inputs folder in case the data is removed or
 # relocated from the URL provided above ####
 write.csv(x = homicides_cleaned,
-          file = "inputs/data/homicides_cleaned.csv")
+          file = here::here("inputs/data/homicides_cleaned.csv"))
 
 write.csv(x = homicides_count,
-          file = "inputs/data/homicides_count.csv")
+          file = here::here("inputs/data/homicides_count.csv"))
 
 write.csv(x = homicides_each_year,
-          file = "inputs/data/homicides_each_year.csv")
+          file = here::here("inputs/data/homicides_each_year.csv"))
 
 write.csv(x = homicides_summary,
-          file = "inputs/data/homicides_summary.csv")
+          file = here::here("inputs/data/homicides_summary.csv"))
 
 write.csv(x = homicides_high,
-          file = "inputs/data/homicides_high.csv")
+          file = here::here("inputs/data/homicides_high.csv"))
 
 write.csv(x = Homicides_vs_division,
-          file = "inputs/data/homicides_vs_division.csv")
+          file = here::here("inputs/data/homicides_vs_division.csv"))
 
 write.csv(x = Homicides_compare,
-          file = "inputs/data/homicides_compare.csv")
+          file = here::here("inputs/data/homicides_compare.csv"))
 
 write.csv(x = Homicides_compare_filtered,
-          file = "inputs/data/Homicides_compare_filtered.csv")
+          file = here::here("inputs/data/Homicides_compare_filtered.csv"))
